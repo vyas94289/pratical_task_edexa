8a179dad055e6760c4ccd07ca4b1554656f71a87
Gaurang Vyas <gaurang.vyas@solutionanalysts.com>
2021-07-07 16:45:49 +0530

    [init] created iOS project

---
+++ pratical_task_edexa/pratical_task_edexa/pratical_task_edexaApp.swift
@@ -0,0 +1,17 @@
+//
+//  pratical_task_edexaApp.swift
+//  pratical_task_edexa
+//
+//  Created by Gaurang Vyas on 07/07/21.
+//
+
+import SwiftUI
+
+@main
+struct pratical_task_edexaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
